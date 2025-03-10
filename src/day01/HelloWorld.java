8d74f39110f7187faa7c71c5981899b14563588e
Melisa <karakoyuncumelisa@gmail.com>
2025-03-10 14:49:34 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,6 +3,9 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello CYDEO");
+        System.out.println("Hello Programmers");
+
     }
 
 }
