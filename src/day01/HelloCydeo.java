8abe2d3e0fb4008cb839abfa9fe4d2196fcd8b40
Melisa <karakoyuncumelisa@gmail.com>
2025-03-10 14:58:56 -0500

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -2,7 +2,10 @@ package day01;
 
 public class HelloCydeo {
     public static void main(String[] args) {
+        
         System.out.println("Hello Cydeo");
+
+        //Some changes are added.
 
     }
 }
